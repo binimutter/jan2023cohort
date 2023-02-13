f56239bb2ffd275dd0b0ee9cafc8c4e2511fc5d0
Ubin Jung <binimutter@gmail.com>
2023-02-13 11:02:43 -0600

    created and completed fundamental 1 assignment

---
+++ assignments/cSharp/week1/FundamentalsOne/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i=1; i<=255; i++) {
+    Console.WriteLine(i);
+}
+
+Random rand = new Random();
+int sum = 0;
+for (int i=1; i<=5; i++) {
+    int random = rand.Next(10,20);
+    Console.WriteLine(random);
+    sum += random;
+}
+Console.WriteLine(sum);
+
+for (int i=1; i<=100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        Console.WriteLine("FizzBuzz");
+        continue;
+    } else if (i % 3 == 0) {
+        Console.WriteLine("Fizz");
+    } else if (i % 5 == 0) {
+        Console.WriteLine("Buzz");
+    }
+}
