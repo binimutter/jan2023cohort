288c4d6b47de3cc647119db9ea655f080685961a
Ubin Jung <binimutter@gmail.com>
2023-02-19 21:20:19 -0600

    completed game developer 1

--- assignments/cSharp/week1/gameDeveloperOne/Program.cs
+++ assignments/cSharp/week1/gameDeveloperOne/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Enemy Thanos = new Enemy("Thanos");
+Attack Punch = new Attack("Punch", 5);
+Attack Kick = new Attack("Kick", 10);
+Attack FingerSnap = new Attack("FingerSnap", 25);
+
+Thanos.AddAttack(Punch);
+Thanos.AddAttack(Kick);
+Thanos.AddAttack(FingerSnap);
+
+Thanos.RandomAttack();
