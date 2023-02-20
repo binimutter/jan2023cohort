d322d57b98683e0fa84d4f97ed715e5904530389
Ubin Jung <binimutter@gmail.com>
2023-02-19 22:41:43 -0600

    completed game developer 2

--- assignments/cSharp/week1/gameDeveloperTwo/Program.cs
+++ assignments/cSharp/week1/gameDeveloperTwo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Melee Stevens = new Melee("Stevens");
+Ranged Bricka = new Ranged("Bricka");
+Magic Yoobie = new Magic("Yoobie");
+
+Stevens.PerformAttack(Bricka, Stevens.AttackList[1]);
+Stevens.PerformAttack(Yoobie, Stevens.Rage());
+
+Bricka.PerformAttack(Stevens, Bricka.AttackList[0]);
+Bricka.Dash();
+Bricka.PerformAttack(Stevens, Bricka.AttackList[0]);
+
+Yoobie.PerformAttack(Stevens, Yoobie.AttackList[0]);
+Yoobie.Heal(Bricka);
+Yoobie.Heal(Yoobie);
