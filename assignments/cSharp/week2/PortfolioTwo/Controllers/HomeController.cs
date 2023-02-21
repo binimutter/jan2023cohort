a00eab101defc896d586bd4231eef6473f485763
Ubin Jung <binimutter@gmail.com>
2023-02-21 00:35:16 -0600

    completed portfolio2

--- assignments/cSharp/week2/PortfolioTwo/Controllers/HomeController.cs
+++ assignments/cSharp/week2/PortfolioTwo/Controllers/HomeController.cs
@@ -1,26 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-namespace ProjectName.Controllers;
+namespace PortfolioTwo.Controllers;
 public class HomeController : Controller
 {
-    // Route declaration Option A
-    // This will go to "localhost:5XXX"
     [HttpGet]
     [Route("")]
     public ViewResult Index()
     {
         return View();
-    }
-    // Route declaration Option B
-    // This will go to "localhost:5XXX/projects"
-    [HttpGet("projects")]
-    public string Projects()
-    {
-        return "These are my projects";
-    }
-
-    [HttpGet("contact")]
-    public string Contact()
-    {
-        return "This is my Contact!";
     }
 }
