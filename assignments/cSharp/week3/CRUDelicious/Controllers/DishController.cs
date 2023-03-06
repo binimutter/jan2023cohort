e1aff1e05809557359d3c7321b4628e873e41f93
Ubin Jung <binimutter@gmail.com>
2023-03-05 19:47:32 -0600

    completed crudelicious

--- assignments/cSharp/week3/CRUDelicious/Controllers/DishController.cs
+++ assignments/cSharp/week3/CRUDelicious/Controllers/DishController.cs
@@ -31,7 +31,7 @@ public class DishController : Controller
 
     [HttpGet("/dish/addDish")]
     public IActionResult AddDish() {
-        return View();
+        return View("addDish");
     }
     [HttpPost("/dish/createDish")]
     public IActionResult CreateDish(Dish d) {
@@ -44,5 +44,48 @@ public class DishController : Controller
         } else {
             return View("AddDish");
         }
+    }
+    [HttpGet("dish/{dishId}/edit")]
+    public IActionResult EditDish(int dishId) {
+        Dish? item = db.Dishes.FirstOrDefault(item => item.DishId == dishId);
+        if (item == null) {
+            return RedirectToAction("dashboard");
+        } else {
+            return View("EditDish", item);
+        }
+    }
+    [HttpPost("dish/{dishId}/update")]
+    public IActionResult UpdateDish(Dish d, int dishId) {
+        if(ModelState.IsValid) {
+            Dish? item = db.Dishes.FirstOrDefault(item => item.DishId == dishId);
+            if(item == null) {
+                Console.WriteLine("Id not valid leave edit page");
+                return RedirectToAction("dashboard");
+            } else {
+                item.Name = d.Name;
+                item.Chef = d.Chef;
+                item.Tastiness = d.Tastiness;
+                item.Calories = d.Calories;
+                item.Description = d.Description;
+                item.UpdatedAt = DateTime.Now;
+
+                db.Dishes.Update(item);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+        } else {
+            Console.WriteLine("validations failed get outta here");
+            return View("EditDish", dishId);
+        }
+    }
+    [HttpGet("dish/{dishId}/delete")]
+    public IActionResult DeleteDish(int dishId) {
+        Dish? item = db.Dishes.FirstOrDefault(item => item.DishId == dishId);
+        if(item != null) {
+            db.Dishes.Remove(item);
+            db.SaveChanges();
+            Console.WriteLine("Entry removed");
+        }
+        return RedirectToAction("Dashboard");
     }
 }
